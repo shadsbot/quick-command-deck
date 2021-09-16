9de860e904f88cbdccb2c8d018969d03a4e97532
Colin Diener <colin@colind.me>
2021-09-16 14:17:31 -0700

    Add rust proto gen

---
+++ management_app/build.rs
@@ -0,0 +1,18 @@
+use protobuf_codegen_pure::Customize;
+
+fn main() {
+    match protobuf_codegen_pure::Codegen::new()
+        .customize(Customize {
+            gen_mod_rs: Some(true),
+            ..Default::default()
+        })
+        .out_dir("src/protos")
+        .include("../")
+        .input("../communique.proto")
+        .run() {
+            Err(e) => {
+                println!("cargo:error={:?}", e)
+            }
+            Ok(_) => {},
+        }
+}
